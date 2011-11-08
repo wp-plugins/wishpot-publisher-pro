5b2f86688b4cb7e392dbee05e42b0356012f18fc
wishpot <wishpot@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-08 08:06:40 +0000

    fixing latest versioning
    git-svn-id: https://plugins.svn.wordpress.org/wishpot-publisher-pro/tags/1.0.6@460503 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,80 @@
+=== Wishpot Publisher Pro&copy; ===
+Contributors: Wishpot Inc
+Author Uri: http://publisher.wishpot.com
+Plugin Uri: http://publisher.wishpot.com/publishers/wordpress
+Tags: import, xml data, wishpot, ad manager, ads, products, money
+Requires at least: 2.7
+Tested up to: WP 3.2
+Stable tag: 1.0.6
+
+
+
+== Description ==
+
+Tested with **Wordpress 3.2**
+
+This free plugin allows you to generate revenues from your blog by integrating products in your pages or sidebar.  You can create any pages by using the built-in short-codes or just use the widgets. 
+The generated widgets differ in size (300x600, 300x250, 160x600, 120x600 etc.).   Products are automatically populated using the Wishpot products database.
+
+For more information about the Wishpot Publisher program you can visit http://publisher.wishpot.com. 
+
+
+If you have problems or questions, please post it in the **[Support forum](http://wordpress.org/tags/wishpot?forum_id=10#postform)** .  
+
+== Support ==
+
+Please post your questions to the Wishpot support forums: http://support.wishpot.com/forums/20359831-publisher-program
+
+
+== Installation ==
+
+1.  extract plugin zip file and load up to your wp-content/plugin directory
+2.  Activate Plugin in the Admin => Plugins Menu
+
+== Frequently Asked Questions ==
+
+= If I try to activate the plugin, I get a 404 error message =
+
+You must login to wordpress with an administrator user.
+
+= other question =
+
+answer !
+
+== Screenshots ==
+
+1. WP Admin Backend - Plugin Option Page
+2. screenshot taken from example widgets with generated product ads in size 300x250 and 300x600
+3. screenshot with example product page - ads generated from the Video Games Category with price and brand filter
+
+
+== Translations ==
+* German (de_DE) (next release)
+* English (default)
+* other must be translated
+
+== Changelog ==
+
+= 1.0.0 =
+- Wishpot Ad Manager Plugin changed first version 
+
+= 1.0.1 =
+- screenshot-1.png changed
+- fix for firefox 4 added
+
+= 1.0.2 =
+- error on product detail page fixed - the buy it now buttons was not working
+
+= 1.0.3 =
+- removed tracking id from plugin settings page
+- language support added for plugin
+
+= 1.0.4 =
+- error in content filter removed
+- debug infos removed
+
+= 1.0.5 =
+- hiding filters until we improve some of the overall behavior
+
+`<?php code(); // goes in backticks ?>`
+
---
+++ wishpot-pub-pro.php
@@ -0,0 +1,385 @@
+<?php
+/*
+Plugin Name: Wishpot Publisher Pro&copy; 
+Plugin URI: http://wishpot.com/wordpress-plugins/wishpot-publisher-pro
+Description: Wishpot Publisher Pro&copy; allows you to easily add product based ads to your wordpress powered website as widget in a sidebar or as pages.
+Version: 1.0.6
+Author: Wishpot Inc
+Author URI: http://wishpot.com
+License: GPL2
+*/
+
+global $wpdb;
+
+define('WISPURL', WP_PLUGIN_URL . '/' . str_replace(basename( __FILE__),"",plugin_basename(__FILE__)) );
+define('WISPDIR', WP_PLUGIN_DIR . '/' . str_replace(basename( __FILE__),"",plugin_basename(__FILE__)) );
+
+$wishpot_plugin_url = WISPURL;
+
+
+// relative path to WP_PLUGIN_DIR where the translation files will sit:
+$plugin_path = dirname(plugin_basename(__FILE__)) . '/lang';
+load_plugin_textdomain( 'wishpot-pub-pro', false, $plugin_path );
+
+
+if ( function_exists('load_plugin_textdomain') ) 
+{
+  if ( !defined('WP_PLUGIN_DIR') ) 
+  {
+    load_plugin_textdomain( 'wishpot-pub-pro', str_replace( ABSPATH, '', dirname(__FILE__)) . '/lang');
+  } 
+  else 
+  {
+    load_plugin_textdomain( 'wishpot-pub-pro', false, dirname(plugin_basename(__FILE__)) . '/lang');
+  }
+}
+
+
+
+include_once( dirname(__FILE__) . '/wishpot-pub-pro-widgets.php');
+include_once( dirname(__FILE__) . '/wishpot-pub-pro-functions.php');
+include_once( dirname(__FILE__) . '/wishpot-pub-pro-option-page.php');
+
+register_activation_hook(   __FILE__, 'wishpot_pub_pro_plugin_activation');   
+register_deactivation_hook( __FILE__, 'wishpot_pub_pro_plugin_deactivation'); 
+
+add_action( 'admin_init',       'wishpot_pub_pro_init_method');
+add_action( 'widgets_init',     create_function('', 'return register_widget("wishpot_pub_pro_Widget");'));
+add_action( 'wp_print_styles',  'wishpot_pub_pro_print_styles');
+add_action( 'wp_print_scripts', 'wishpot_pub_pro_print_scripts');
+add_action( 'admin_menu',       'wishpot_pub_pro_plugin_add_option_page');
+add_action( 'admin_head',       'wishpot_pub_pro_plugin_load_header_tags');
+add_action( 'wp_footer',        'wishpot_pub_pro_wp_footer');
+add_filter( 'the_content',      'wishpot_pub_pro_content');
+
+//  ajax calls
+add_action( 'wp_ajax_wishpot_pub_pro_ajax_action', 'wishpot_pub_pro_ajax_func_get_results');
+add_action( 'wp_ajax_nopriv_wishpot_pub_pro_ajax_action', 'wishpot_pub_pro_ajax_func_get_results');
+
+
+////////////////////////////////////////////////////////////////////////////////
+// plugin activation hook
+////////////////////////////////////////////////////////////////////////////////
+function wishpot_pub_pro_plugin_activation() 
+{
+
+/* Pre-installed categories have been disabled for now - 07-21-2011 Fast_Websites
+	//  check if default cat exists
+  if ( !get_cat_ID('Wispot Publisher Pro') ) 
+  {
+    wp_create_category( 'Wishpot Publisher Pro' );
+  }
+  wishpot_pub_pro_reload_category();
+*/
+
+  add_option( 'wishpot_pub_pro_options', array(), '', 'no');
+  wishpot_pub_pro_set_option_defaults();
+
+  wishpot_pub_pro_migrate_old_options();
+  wishpot_pub_pro_add_sample_page();
+  
+  //  check if product detail page exists
+  wishpot_pub_pro_create_product_search_results_page();
+
+  return; 
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// plugin deactivation hook
+////////////////////////////////////////////////////////////////////////////////
+function wishpot_pub_pro_plugin_deactivation() 
+{
+  wp_clear_scheduled_hook('scheduled_import_articles_custom_hook');
+
+  delete_option('wishpot_pub_pro_options');
+
+  return;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// add plugin option page
+////////////////////////////////////////////////////////////////////////////////
+function wishpot_pub_pro_plugin_add_option_page()
+{
+  add_menu_page(    'Wishpot Plugin Options',      'Wishpot', 'manage_options', 'wishpot-pub-pro-option-page', 'wishpot_pub_pro_plugin_create_option_page', WISPURL . 'img/wishpot_favicon.ico');
+//  add_submenu_page( 'wishpot-pub-pro-option-page', 'About',             'About',            'manage_options', 'wishpot-pub-pro-about', 'wishpot_pub_pro_display_plugin_about');
+//  add_submenu_page( 'wishpot-pub-pro-option-page', 'Wishpot Center',    'Wishpot Center',   'manage_options', 'wishpot-sub-page', 'wishpot_pub_pro_javascript_to_redirect_to_wishpotcenter');
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// load plugin wp-admin css and js
+////////////////////////////////////////////////////////////////////////////////
+function wishpot_pub_pro_plugin_load_header_tags()
+{
+  $js_number_categories = get_categories(array('hide_empty' => false));
+  $js_category_count=0;
+  foreach ($js_number_categories as $category) 
+    $js_category_count++;
+
+  echo 	"\n\n";
+  echo 	'<!-- Wishpot Publisher Pro - Plugin Option CSS -->' . "\n";
+  echo 	'<link rel="stylesheet" type="text/css" media="all" href="' . WISPURL . 'css/plugin-option.php" />';
+	
+  return;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// plugin init method
+////////////////////////////////////////////////////////////////////////////////
+function wishpot_pub_pro_init_method()
+{
+  if ( get_magic_quotes_gpc() ) 
+  {
+    $_POST      = array_map( 'stripslashes_deep', $_POST );
+    $_GET       = array_map( 'stripslashes_deep', $_GET );
+    $_COOKIE    = array_map( 'stripslashes_deep', $_COOKIE );
+    $_REQUEST   = array_map( 'stripslashes_deep', $_REQUEST );
+  }
+
+  wp_enqueue_script('dashboard');
+  wp_enqueue_script('postbox');
+  wp_enqueue_script('jquery-ui-resizable');
+  wp_enqueue_script('jquery-ui-droppable');
+  wp_enqueue_script('wp-ajax-response');
+
+  $wishpot_pub_pro_products_cache = array();
+  $wishpot_pub_pro_ads_cache = array();
+  wp_cache_set('wishpot_pub_pro_products', $wishpot_pub_pro_products_cache);
+  wp_cache_set('wishpot_pub_pro_ads', $wishpot_pub_pro_ads_cache);
+
+  return;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// plugin options functions
+////////////////////////////////////////////////////////////////////////////////
+function wishpot_pub_pro_get_option($field) 
+{
+  if (!$options = wp_cache_get('wishpot_pub_pro_options')) 
+  {
+    $options = get_option('wishpot_pub_pro_options');
+    wp_cache_set('wishpot_pub_pro_options',$options);
+  }
+  return $options[$field];
+}
+
+function wishpot_pub_pro_update_option($field, $value) 
+{
+  wishpot_pub_pro_update_options(array($field => $value));
+}
+
+function wishpot_pub_pro_update_options($data) 
+{
+  $options = array_merge(get_option('wishpot_pub_pro_options'),$data);
+  update_option('wishpot_pub_pro_options',$options);
+  wp_cache_set('wishpot_pub_pro_options',$options);
+}
+
+function wishpot_pub_pro_migrate_old_options() 
+{
+  global $wpdb;
+
+  //  check for a old Option
+  if ( (get_option('wishpot_pub_pro_old_option') === false) ) 
+  {
+    return;
+  }
+
+  $old_fields = array(
+       'wishpot_pub_pro_product_detail_page_id'   => NULL,
+       'wishpot_pub_pro_widget_ga'         => 'off',
+       'wishpot_pub_pro_pp_fblike'        => 'off',
+       'wishpot_pub_pro_pp_fbshare'       => 'off',
+       'wishpot_pub_pro_pp_twshare'       => 'off'
+       );
+
+  $new_fields = array(
+       'wishpot_pub_pro_product_detail_page_id'   => NULL,
+       'wishpot_pub_pro_widget_ga'         => 'off',
+       'wishpot_pub_pro_pp_fblike'        => 'off',
+       'wishpot_pub_pro_pp_fbshare'       => 'off',
+       'wishpot_pub_pro_pp_twshare'       => 'off'
+       );
+
+  foreach($old_fields as $index=>$field) 
+  {
+    if ( $index == 3 )
+    {
+      $cats = get_option($old_fields[$index]);
+      if ( is_array($cats) )
+        wishpot_pub_pro_update_option($new_fields[$index], $cats);
+      else
+        wishpot_pub_pro_update_option($new_fields[$index], array($cats));
+    }
+    else
+      wishpot_pub_pro_update_option($new_fields[$index], get_option($old_fields[$index]));
+    delete_option($old_fields[$index]);
+  }
+  $wpdb->query("OPTIMIZE TABLE `" . $wpdb->options . "`");
+
+  return;
+}
+
+function wishpot_pub_pro_set_option_defaults()
+{
+  $current_user_id=1;
+  global $current_user;    
+  get_currentuserinfo();
+
+  if ( $current_user->ID != '' ) 
+    $current_user_id=$current_user->ID;
+
+  $default_options = array(
+       'wishpot_pub_pro_product_detail_page_id'   => NULL,
+       'wishpot_pub_pro_product_search_results_page_id'   => NULL,
+       'wishpot_pub_pro_pagesize'                 => '18',
+       'wishpot_pub_pro_widget_ga'                => 'off',
+       'wishpot_pub_pro_pp_fblike'                => 'off',
+       'wishpot_pub_pro_pp_fbshare'               => 'off',
+       'wishpot_pub_pro_pp_twshare'               => 'off',
+	   // Default options - 07-21-2011 Fast_Websites
+	   'wishpot_pub_pro_default_cat'               => 'Video Games',
+	   'wishpot_pub_pro_default_keywords'               => 'xbox',
+	   'wishpot_pub_pro_default_place_ID'               => '6',
+        );
+
+  $wishpot_pub_pro_options = get_option('wishpot_pub_pro_options');
+
+  foreach ($default_options as $def_option => $value )
+  {
+    if ( !$wishpot_pub_pro_options[$def_option] )
+    {
+      wishpot_pub_pro_update_option( $def_option, $value );
+    }
+  }
+
+  return;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// print plugin option page and check post data
+////////////////////////////////////////////////////////////////////////////////
+function wishpot_pub_pro_plugin_create_option_page()
+{
+  if ( $_POST['wishpot_pub_pro_add_cat_btn'] )
+  {
+    wishpot_pub_pro_add_category();
+  }
+  if ( $_POST['wishpot_pub_pro_reload_cats_btn'] )
+  {
+    wishpot_pub_pro_reload_category();
+  }
+
+  if ( $_POST['wishpot_pub_pro_update_options_btn'] )
+  {
+    wishpot_pub_pro_save_plugin_options();
+
+    echo '<div id="message" class="updated fade">';
+    echo '<strong>Plugin Settings saved !!!</strong></div>';
+  }
+
+
+  wishpot_pub_pro_plugin_print_option_page();
+
+  return;
+}
+
+
+function wishpot_pub_pro_add_sample_page()
+{
+// adds explainatory sample product page for user
+// 07-21-2011 Fast_Websites
+$sample_post = array(
+ 'post_title' => 'Sample Wishpot Product Page',
+ 'post_content' => '[wishpot_ads_product_page]',
+ 'post_status' => 'publish',
+ 'post_author' => 1,
+);
+$samplePostID = wp_insert_post($sample_post);
+$cmntData = array(
+    'comment_post_ID' => $samplePostID,
+    'comment_author' => 'Wishpot Publisher Pro',
+    'comment_author_url' => 'http://wishpot.com/wordpress-plugins/wishpot-publisher-pro',
+    'comment_content' => 'This is a sample Wishpot Product Page you can customize using the Wishpot Publisher Pro plug-in.  You can create a product page like this one just by creating a Wordpress post and adding the appropriate shortcodes to your post.  You can also add Wishpot widgets to your entire website by going to Appearance -> Widgets in your Wordpress dashboard.<br /><br />
+	The shortcodes are:
+	[wishpot_ads_product_page]<br />
+Required.  Declares a page to be a Wishpot Product Page.<br /><br />
+[wishpot_ads_category=Your Category Here]<br />
+Optional.  Defines the category to limit Wishpot results.<br /><br />
+[wishpot_ads_keywords=Your Keywords Here]<br />
+Optional.  Defines the keywords to limit Wishpot results.<br /><br />
+[wishpot_ads_placement_id=6]<br />
+Required.  Defines the Placement ID for Wishpot results.<br /><br />
+[wishpot_ads_source=Your Source Here]<br />
+Optional.  Sends the source name to Wishpot.<br /><br />
+Default listing criteria can be set in the Wishpot options menu.',
+    'comment_parent' => 0,
+    'user_id' => 1,
+    'comment_author_IP' => '127.0.0.1',
+    'comment_agent' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.10) Gecko/2009042316 Firefox/3.0.10 (.NET CLR 3.5.30729)',
+    'comment_date' => current_time('mysql'),
+    'comment_approved' => 1,
+);
+wp_insert_comment($cmntData);
+}
+
+
+function wishpot_pub_pro_is_min_wp($version) 
+{
+  return version_compare( $GLOBALS['wp_version'], $version. 'alpha', '>=');
+}
+
+
+
+function wishpot_pub_pro_print_styles()
+{
+  if ( is_admin() )
+  {
+  }
+  else
+  {
+    wp_register_style('wishpot_pub_pro_frontend_styles', WISPURL . 'css/wishpot-pub-pro-frontend-plugin.php');
+    wp_register_style('wishpot_pub_pro_widget_styles', WISPURL . 'css/wishpot-pub-pro-widget.php');
+    wp_enqueue_style( 'wishpot_pub_pro_frontend_styles' );
+    wp_enqueue_style( 'wishpot_pub_pro_widget_styles' );
+  }
+
+  return;
+}
+
+
+function wishpot_pub_pro_print_scripts()
+{
+  global $wishpot_plugin_url;
+
+  if ( is_admin() )
+  {
+  }
+  else
+  {
+    //  print scripts for the public and frontend
+    wp_enqueue_script(jquery);
+    wp_enqueue_script(json);
+    wp_enqueue_script(json2);
+    wp_enqueue_script( 'wishpot_pub_pro_css_browser_script', plugin_dir_url( __FILE__ ) . 'js/css_browser_selector.js', false, false, false);
+    wp_enqueue_script( 'wishpot_pub_pro_script', plugin_dir_url( __FILE__ ) . 'js/wishpot_pub_pro.js', array('jquery'), false, false);
+    echo
+    '
+      <script type="text/javascript" language="javascript">
+      //<![CDATA[
+         var wishpot_plugin_url = "' . $wishpot_plugin_url . '";
+         var ajaxurl  = "' . admin_url('admin-ajax.php') . '";
+      //]]>
+      </script>
+    ';
+  }
+
+  return;
+}
+
+
+?>
